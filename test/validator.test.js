ecb1e211ca4231de4e44a689d6ae2322b002eabb
Brian J Brennan <brianloveswords@gmail.com>
2013-02-20 18:02:16 -0500

    More refactorin'

--- test/validator.test.js
+++ test/validator.test.js
@@ -74,130 +74,144 @@ const BADGE_GENERATORS = {
 };
 
 
-const BAD_STRINGS = [
-  ['not', 'a', 'string'],
-  { not: 'a string' },
-];
-const GOOD_STRINGS = [
-  'OH', 'hey'
-];
-const BAD_EMAILS = [
-  'lkajd',
-  'skj@asdk',
-  '@.com',
-  '909090',
-  '____!@',
-];
-const GOOD_EMAILS = [
-  'brian@awesome.com',
-  'yo+wut@example.com',
-  'ümlaut@heavymetal.de',
-];
-const GOOD_HASHES = [
-  'sha1$c0b19425e0f2c8021ab06c79b19144e127b0f2cb',
-  'sha256$406f04039d10c79c070b26781e8246dc01ed1d0453c5ad0fa705ff7d507fd898'
-];
-const BAD_HASHES = [
-  'sha1stuff',
-  'bcrypt$5$something'
-];
-const GOOD_URLS = [
-  'http://example.com/',
-  'https://example.com/w/yo',
-  '/partial/path',
-  '/rad.awesome/great/',
-  '/foreign/crázy/ååú´¨la/'
-];
-const BAD_URLS = [
-  '-not-asdo',
-  'ftp://bad-scheme',
-  '@.com:90/',
-  'just totally wrong'
-];
-const GOOD_TIMES = [
-  Date.now()/1000 | 0,
-  '2012-01-01'
-];
-const BAD_TIMES = [
-  'oiajsd09gjas;oj09',
-  'foreever ago',
-  '@.com:90/',
-  '2001-10-190-19',
-  '901d1',
-  '000000000000000000000'
-];
-const GOOD_OBJECTS = [
-  { something: 'yes' },
-  { other: { thing: 'no' }}
-];
-const BAD_OBJECTS = [
-  [ 1, 2, 3, 4 ],
-  'just a string',
-  function () {},
-];
-const BAD_VERSIONS = [
-  'v100',
-  '50',
-  'v10.1alpha',
-  '1.2.x'
-];
-const GOOD_VERSIONS = [
-  '0.1.1',
-  '2.0.1',
-  '1.2.3',
-  'v1.2.1'
-];
-const GOOD_ORIGINS = [
-  'http://example.com',
-  'https://example.com:80',
-  'https://www.example.com',
-  'https://www.example.com:8080',
-  'http://example.com/'
-];
-const BAD_ORIGINS = [
-  '-not-asdo',
-  'ftp://bad-scheme',
-  '@.com:90/',
-  'just totally wrong',
-  'http://example.com/what',
-  'http://example.com:8080/false'
-];
+const STRINGS = {
+  good: ['OH', 'hey'],
+  bad: [
+    ['not', 'a', 'string'],
+    { not: 'a string' },
+  ]
+};
+const EMAILS = {
+  good: [
+    'brian@awesome.com',
+    'yo+wut@example.com',
+    'ümlaut@heavymetal.de',
+  ],
+  bad: [
+    'lkajd',
+    'skj@asdk',
+    '@.com',
+    '909090',
+    '____!@',
+  ]
+};
+const HASHES = {
+  good: [
+    'sha1$c0b19425e0f2c8021ab06c79b19144e127b0f2cb',
+    'sha256$406f04039d10c79c070b26781e8246dc01ed1d0453c5ad0fa705ff7d507fd898'
+  ],
+  bad: [
+    'sha1stuff',
+    'bcrypt$5$something'
+  ]
+}
+const URLS = {
+  good: [
+    'http://example.com/',
+    'https://example.com/w/yo',
+    '/partial/path',
+    '/rad.awesome/great/',
+    '/foreign/crázy/ååú´¨la/'
+  ],
+  bad: [
+    '-not-asdo',
+    'ftp://bad-scheme',
+    '@.com:90/',
+    'just totally wrong'
+  ],
+};
+const TIMES = {
+  good : [
+    Date.now()/1000 | 0,
+    '2012-01-01'
+  ],
+  bad: [
+    'oiajsd09gjas;oj09',
+    'foreever ago',
+    '@.com:90/',
+    '2001-10-190-19',
+    '901d1',
+    '000000000000000000000'
+  ]
+};
+const OBJECTS = {
+  good: [
+    { something: 'yes' },
+    { other: { thing: 'no' }}
+  ],
+  bad: [
+    [ 1, 2, 3, 4 ],
+    'just a string',
+    function () {},
+  ]
+};
+const VERSIONS = {
+  good: [
+    '0.1.1',
+    '2.0.1',
+    '1.2.3',
+    'v1.2.1'
+  ],
+  bad: [
+    'v100',
+    '50',
+    'v10.1alpha',
+    '1.2.x'
+  ]
+};
+const ORIGINS = {
+  good: [
+    'http://example.com',
+    'https://example.com:80',
+    'https://www.example.com',
+    'https://www.example.com:8080',
+    'http://example.com/'
+  ],
+  bad : [
+    '-not-asdo',
+    'ftp://bad-scheme',
+    '@.com:90/',
+    'just totally wrong',
+    'http://example.com/what',
+    'http://example.com:8080/false'
+  ]
+};
 
 const VALID =  {
-  recipient: [GOOD_EMAILS, GOOD_HASHES],
-  salt: [GOOD_STRINGS],
-  evidence: [GOOD_URLS],
-  expires: [GOOD_TIMES],
-  issued_on: [GOOD_TIMES],
-  badge: [GOOD_OBJECTS],
-  'badge.version': [GOOD_VERSIONS],
-  'badge.name': [GOOD_STRINGS],
-  'badge.description': [GOOD_STRINGS],
-  'badge.image': [GOOD_URLS],
-  'badge.criteria': [GOOD_URLS],
-  'badge.issuer': [GOOD_OBJECTS],
-  'badge.issuer.name': [GOOD_STRINGS],
-  'badge.issuer.contact': [GOOD_EMAILS],
-  'badge.issuer.origin': [GOOD_ORIGINS],
-  'badge.issuer.org': [GOOD_STRINGS],
+  recipient: [EMAILS.good, HASHES.good],
+  salt: [STRINGS.good],
+  evidence: [URLS.good],
+  expires: [TIMES.good],
+  issued_on: [TIMES.good],
+  badge: [OBJECTS.good],
+  'badge.version': [VERSIONS.good],
+  'badge.name': [STRINGS.good],
+  'badge.description': [STRINGS.good],
+  'badge.image': [URLS.good],
+  'badge.criteria': [URLS.good],
+  'badge.issuer': [OBJECTS.good],
+  'badge.issuer.name': [STRINGS.good],
+  'badge.issuer.contact': [EMAILS.good],
+  'badge.issuer.origin': [ORIGINS.good],
+  'badge.issuer.org': [STRINGS.good],
 };
 const INVALID = {
-  recipient: [BAD_STRINGS, BAD_EMAILS, BAD_HASHES],
-  salt: [BAD_STRINGS],
-  evidence: [BAD_STRINGS, BAD_URLS],
-  expires: [BAD_STRINGS, BAD_TIMES],
-  issued_on: [BAD_STRINGS, BAD_TIMES],
-  badge: [BAD_OBJECTS],
-  'badge.version': [BAD_STRINGS, BAD_VERSIONS],
-  'badge.name': [BAD_STRINGS],
-  'badge.description': [BAD_STRINGS],
-  'badge.image': [BAD_STRINGS, BAD_URLS],
-  'badge.criteria': [BAD_STRINGS, BAD_URLS],
-  'badge.issuer': [BAD_OBJECTS],
-  'badge.issuer.name': [BAD_STRINGS],
-  'badge.issuer.contact': [BAD_STRINGS, BAD_EMAILS],
-  'badge.issuer.origin': [BAD_STRINGS, BAD_ORIGINS],
-  'badge.issuer.org': [BAD_STRINGS],
+  recipient: [STRINGS.bad, EMAILS.bad, HASHES.bad],
+  salt: [STRINGS.bad],
+  evidence: [STRINGS.bad, URLS.bad],
+  expires: [STRINGS.bad, TIMES.bad],
+  issued_on: [STRINGS.bad, TIMES.bad],
+  badge: [OBJECTS.bad],
+  'badge.version': [STRINGS.bad, VERSIONS.bad],
+  'badge.name': [STRINGS.bad],
+  'badge.description': [STRINGS.bad],
+  'badge.image': [STRINGS.bad, URLS.bad],
+  'badge.criteria': [STRINGS.bad, URLS.bad],
+  'badge.issuer': [OBJECTS.bad],
+  'badge.issuer.name': [STRINGS.bad],
+  'badge.issuer.contact': [STRINGS.bad, EMAILS.bad],
+  'badge.issuer.origin': [STRINGS.bad, ORIGINS.bad],
+  'badge.issuer.org': [STRINGS.bad],
 };
 
 function flatten(arry) {
