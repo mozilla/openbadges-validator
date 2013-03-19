f61e49087286e96ef6a72c28de7a32f7d6f1fed1
Brian J Brennan <brianloveswords@gmail.com>
2013-03-19 12:15:54 -0400

    Reinstate origin test.

--- test/validator.test.js
+++ test/validator.test.js
@@ -53,8 +53,8 @@ test('0.5.0 badges with errors', function (t) {
     optional('badge.version');
 
     required('badge.issuer.name');
+    required('badge.issuer.origin');
     optional('badge.issuer.contact');
-    optional('badge.issuer.origin');
     optional('badge.issuer.org');
   }
   t.end();
