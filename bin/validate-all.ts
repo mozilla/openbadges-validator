b609b1e7868c07101e9aba042bbee40c5b98de85
oren <get.oren@gmail.com>
2016-01-19 19:50:54 -0800

    Refactor...

--- bin/validate-all.ts
+++ bin/validate-all.ts
@@ -2,91 +2,55 @@
 const validator = require('../');
 const resources = require('../lib/resources');
 const jsonld = require('jsonld');
+var util = require('util');
 var input = process.argv.slice(2)[0];
 var SPEC_VERSIONS = ['0.5.0', '1.0.0', '1.1.0', '2.0.0'];
 
-var validate = {
-  '0.5.0': function (assertion) {
-    return new ValidatorResponse(false, 'None', '');
-  },
-  '1.0.0': function (assertion) {
-    return new ValidatorResponse(false, 'None', '');
-  },
-  '1.1.0': function (assertion) {
-    return new ValidatorResponse(false, 'None', '');
-  },
-  '2.0.0': function (assertion) {
-    return new ValidatorResponse(false, 'Unavailable', '');
+class BadgeAssertion {
+  structuresValid
+  constructor(public body: string) {
+    this.validateStructures = {
+      '0.5.0': function () {
+        return new ValidatorResponse(false, 'None', '');
+      },
+      '1.0.0': function () {
+        return new ValidatorResponse(false, 'None', '');
+      },
+      '1.1.0': function () {
+        return new ValidatorResponse(false, 'None', '');
+      },
+      '2.0.0': function () {
+        return new ValidatorResponse(false, 'Unavailable', '');
+      }
+    }
+  }
+
+  this.toCsvRow = function() {
+
   }
 }
 
-class FullValidationResponse {
-  inputType: string;
-  assertion: Assertion;
-  response: Object;
-
-  constructor(public input: string) {
-    this.response = {};
-    this.assertion = new Assertion(input);
-    if (this.assertion.inputType == 'Unknown') {
-      return;
-    }
-    for (var i = 0; i < SPEC_VERSIONS.length; i++) {
-      var version = SPEC_VERSIONS[i];
-      this.response[version] = new ValidationTest(this.assertion.body, validate[version]);
-    }
-  };
-
-  toCsvRow() {
-    var values = [this.assertion.raw, this.assertion.inputType, this.assertion.verifyUrl, this.assertion.body, this.assertion.fetchError];
-    if (this.assertion.inputType == 'Unknown') {
-      for (var i = 0; i < SPEC_VERSIONS.length; i++) {
-        values.push('FAIL');
-        values.push('Unknown assertion format');
-      }
-    }
-    else {
-      for (var i = 0; i < SPEC_VERSIONS.length; i++) {
-        var version = SPEC_VERSIONS[i];
-        values.push((this.response[version].valid) ? 'OKAY' : 'FAIL');
-        values.push(this.response[version].reason);
-      }
-    }
-    return '"' + values.join('","') + '"\n';
-  };
-}
-
-class Assertion {
-  raw: string;
-  inputType: string;
-  verifyUrl: string;
-  body: string;
-  fetchError: string;
-  constructor(public input: string) {
-    this.raw = input;
-    this.body = '';
-    this.verifyUrl = '';
-    this.fetchError = '';
-    this.inputType = (isJson(this.raw) ? 'JSON' : (isUrl(this.raw) ? 'URL': 'Unknown'));
-    if (this.inputType == 'URL')
-      this.verifyUrl = this.raw;
-    if (this.inputType == 'JSON')
-      this.verifyUrl = getVerifyUrl(JSON.parse(this.raw));
-    if (!this.verifyUrl.length) {
-      return;
-    }
-    const options = {url: this.verifyUrl, json: true, required: true};
+function fetchVerifyUrl (url) {
+  return new Promise(function (resolve, reject) {
+    const options = {url: url, json: true, required: true};
     resources.getUrl(options, function (ex, result) {
       if (result.error) {
-        this.fetchError = result.error;
-        return;
+        reject(result.error);
       }
-      this.body = result.body;
+      resolve(result.body);
     });
-  }
+  });
 }
 
-function getVerifyUrl (json) {
+function parseVerifyUrl (rawInput) {
+  if (isUrl(rawInput))
+    return rawInput;
+  if isJson(rawInput)
+    return extractVerifyUrl(rawInput);
+  return '';
+}
+
+function extractVerifyUrl (json) {
   try {
     var url = json.verify.url;
     if (isUrl(url))
@@ -105,37 +69,31 @@ function isJson (str) {
   try { JSON.parse(str); return true } catch(e) { return false }
 }
 
-interface Validator {
-    (input: string): ValidatorResponse;   
-}
-
-class ValidatorResponse {
-  constructor(public valid: boolean, public reason: string, public details: string) {
-    this.valid = valid;
-    this.reason = reason;
-    this.details = details;
+function validateInput (input, handleOutput) {
+  //var response = new FullValidationResponse(input);
+  var verifyUrl = parseVerifyUrl(input);
+  if (verifyUrl.length) {
+    fetchAssertion(verifyUrl).then(function(response) {
+      if (isJson(response)) {
+        var assertion = new BadgeAssertion(JSON.parse(response));
+        assertion[handleOutput]();
+      }
+      else {
+        handleOutput(input, response, 'Not a valid JSON document.');
+      }
+    }, function(error) {
+      handleOutput(input, verifyUrl, 'Error fetching assertion from verifier IRI');
+    });
+  }
+  else {
+    handleOutput(input, null, 'Not a valid URL');
   }
 }
 
-class ValidationTest {
-  valid: boolean;
-  reason: string;
-  details: string;
-  constructor(public assertion: string, public validate: Validator) {
-    var response = validate(assertion);
-    this.valid = response.valid;
-    this.reason = response.reason;
-    this.details = response.details;
-  };
-}
-
-function validateAll (input, handleResponse) {
-  var response = new FullValidationResponse(input);
-  console.log(response[handleResponse]());
+function toCsvRow(raw, processed, messages) {
+  
 }
 
 (function main () {
-  // Header:
-  //console.log("Raw input, input type, verify URL, fetch body, fetch error, [tests 0.5.0 - 2.0.0]");
-  validateAll(input, 'toCsvRow');
+  validateInput(input, 'toCsvRow');
 })();
